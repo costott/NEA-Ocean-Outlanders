556d14393b193c991fe15a3846a53762bcc529fb
Connor Stott <connorbstott@gmail.com>
2022-09-14 19:01:35 +0100

    finish switching states

--- player_boat.py
+++ player_boat.py
@@ -1,5 +1,6 @@
 import pygame
 
+from boat_feature import BoatFeature
 from boat import Boat, BoatImage
 from cannon import Cannon
 import settings
@@ -11,26 +12,27 @@ class PlayerBoat(Boat):
         super().__init__(groups, start_pos)
         self.z = 1
 
-        self.state = "cannons" # current state of the boat (switching, steering, sailing, cannons)
+        self.state = "sailing" # current state of the boat (switching, steering, sailing, cannons)
 
-        self.cannons.append(Cannon(self, (15,20), True, self.active_cannon0))
-        self.cannons.append(Cannon(self, (-15, 20), False, self.active_cannon1))
-        self.active_cannon = self.cannons[1] # cannon being controlled when in the cannons state
+        self.cannons.append(Cannon(self, (15,20), True, self.active_cannon0))    # left cannon
+        self.cannons.append(Cannon(self, (-15, 20), False, self.active_cannon1)) # right cannon
 
         self.get_sails()
+
+        main_sail_feature = BoatFeature("main_sail_blue_small.png", (0,-7), self.start_sailing)
+        steering_wheel_featue = BoatFeature("steering_wheel.png", (0,-36), self.start_steering)
+        self.boat_features = [main_sail_feature, steering_wheel_featue] # group non-cannon features together
+
         # all boat images to be used to compile main image
-        self.images = [self.hull] + self.cannons + self.sails 
+        self.images = [self.hull] + self.cannons + self.boat_features + self.sails 
         self.make_main_boat_image()
 
         self.max_speed = settings.BOAT_BASE_SPEED # maxmim speed the boat can move
+        self.speed = 0             # speed starts at 0
 
         self.holding_space = False # if space is being held to make sure its lifted before acting again
 
         self.switch_timer = 0      # timer to count down when entering/exiting the switching state
-
-        # FOR TESTING CANNON AIMING
-        self.speed = 0
-        self.angle_velocity = 5
     
     def get_sails(self) -> None:
         """get all player boat sail images"""
@@ -112,10 +114,15 @@ class PlayerBoat(Boat):
             # decrease sail alpha
             sail.alpha -= ((255-settings.PB_SAIL_HIDDEN_ALPHA)/settings.PB_STATE_SWITCH_TIME) * 1/tools.get_fps()
         
+        for feature in self.boat_features:
+            # increase feature alphas
+            feature.alpha += (255/settings.PB_STATE_SWITCH_TIME) * 1/tools.get_fps()
+        
         self.switch_timer -= 1/tools.get_fps()
         if self.switch_timer <= 0:
             self.switch_timer = 0 # stop timer
             for sail in self.sails: sail.alpha = settings.PB_SAIL_HIDDEN_ALPHA # make sure alphas are correct
+            for feature in self.boat_features: feature.alpha = 255             
             self.state = "switching" # enter switching state
         
         self.make_main_boat_image() # remake image as alphas have changed
@@ -126,10 +133,15 @@ class PlayerBoat(Boat):
             # increase sail alpha
             sail.alpha += ((255-settings.PB_SAIL_HIDDEN_ALPHA)/settings.PB_STATE_SWITCH_TIME) * 1/tools.get_fps()
         
+        for feature in self.boat_features:
+            # decrease feature alphas
+            feature.alpha -= (255/settings.PB_STATE_SWITCH_TIME) * 1/tools.get_fps()
+        
         self.switch_timer -= 1/tools.get_fps()
         if self.switch_timer <= 0:
             self.switch_timer = 0 # stop timer
             for sail in self.sails: sail.alpha = 255 # make sure alphas are correct
+            for feature in self.boat_features: feature.alpha = 0             
             self.state = self.state[15:len(self.state)] # go to next state
         
         self.make_main_boat_image() # remake image as alphas have changed
@@ -138,6 +150,8 @@ class PlayerBoat(Boat):
         """logic whilst in the switching state"""
         for cannon in self.cannons: 
             cannon.feature_update()
+        for feature in self.boat_features:
+            feature.feature_update()
 
         self.make_main_boat_image() # remake images as sizes may change
     
@@ -152,3 +166,13 @@ class PlayerBoat(Boat):
         self.state = "exit switching-cannons"             # exit switching state
         self.switch_timer = settings.PB_STATE_SWITCH_TIME # start switch timer
         self.active_cannon = self.cannons[1]              # set active cannon
+
+    def start_sailing(self) -> None:
+        """switches to sailing state"""
+        self.state = "exit switching-sailing"             # exit switching state
+        self.switch_timer = settings.PB_STATE_SWITCH_TIME # start switch timer
+    
+    def start_steering(self) -> None:
+        """switches to steering state"""
+        self.state = "exit switching-steering"            # exit switching state
+        self.switch_timer = settings.PB_STATE_SWITCH_TIME # start switch timer
