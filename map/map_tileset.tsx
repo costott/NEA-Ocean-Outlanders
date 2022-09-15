45c116062192778cf56efb9642d3532b91eedbe9
Connor Stott <connorbstott@gmail.com>
2022-09-15 19:09:53 +0100

    make map

---
+++ map/map_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="map tileset" tilewidth="64" tileheight="64" tilecount="96" columns="16">
+ <image source="../../../../../Downloads/kenney_piratepack/Tilesheet/tiles_sheet.png" width="1024" height="384"/>
+</tileset>
